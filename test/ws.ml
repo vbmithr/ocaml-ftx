ac959396465335a16eda79d02ac19c043a5bb46e
Vincent Bernardoff <vb@luminar.eu.org>
2019-06-23 00:27:48 +0200

    test: automatically find symbols

--- test/ws.ml
+++ test/ws.ml
@@ -7,19 +7,40 @@ open Ftx_ws_async
 let src = Logs.Src.create "ftx.ws-test"
     ~doc:"Ftx API - WS test application"
 
+let markets = ref []
+
 let process_user_cmd w =
   let process s =
     match String.split s ~on:' ' with
-    | "ticker" :: sym :: _ ->
-      Pipe.write w (subscribe Ticker sym)
-    | "trades" :: sym :: _ ->
-      Pipe.write w (subscribe Trades sym)
-    | "books" :: sym :: _ ->
-      Pipe.write w (subscribe Orderbook sym)
-    | "untrades" :: sym :: _ ->
-      Pipe.write w (unsubscribe Trades sym)
-    | "unbooks" :: sym :: _ ->
-      Pipe.write w (unsubscribe Orderbook sym)
+    | "all" :: _ ->
+      Deferred.List.iter !markets ~f:begin fun sym ->
+        Pipe.write w (subscribe Trades sym) >>= fun () ->
+        Pipe.write w (subscribe Orderbook sym)
+      end
+    | "tickers" :: _ ->
+      Deferred.List.iter !markets ~f:begin fun sym ->
+        Pipe.write w (subscribe Ticker sym)
+      end
+    | "ticker" :: syms ->
+      Deferred.List.iter syms ~f:begin fun sym ->
+        Pipe.write w (subscribe Ticker sym)
+      end
+    | "trades" :: syms ->
+      Deferred.List.iter syms ~f:begin fun sym ->
+        Pipe.write w (subscribe Trades sym)
+      end
+    | "books" :: syms ->
+      Deferred.List.iter syms ~f:begin fun sym ->
+        Pipe.write w (subscribe Orderbook sym)
+      end
+    | "untrades" :: syms ->
+      Deferred.List.iter syms ~f:begin fun sym ->
+        Pipe.write w (unsubscribe Trades sym)
+      end
+    | "unbooks" :: syms ->
+      Deferred.List.iter syms ~f:begin fun sym ->
+        Pipe.write w (unsubscribe Orderbook sym)
+      end
     (* | "unsubscribe" :: chanid :: _ ->
      *   let chanid = int_of_string chanid in
      *   Pipe.write w (Unsubscribe { chanid ; reqid = None })
@@ -45,14 +66,19 @@ let process_user_cmd w =
   loop ()
 
 let main () =
-  with_connection begin fun r w ->
-    let log_incoming msg =
-      Logs_async.debug ~src (fun m -> m "%a" pp msg) in
-    Deferred.all_unit [
-      process_user_cmd w ;
-      Pipe.iter r ~f:log_incoming
-    ]
-  end
+  Fastrest.request Ftx_rest.markets >>= function
+  | Error e ->
+    failwith Format.(asprintf "%a" (Fastrest.pp_print_error pp_print_string) e)
+  | Ok mkts ->
+    markets := List.map mkts ~f:(fun { name; _ } -> name) ;
+    with_connection begin fun r w ->
+      let log_incoming msg =
+        Logs_async.debug ~src (fun m -> m "%a" pp msg) in
+      Deferred.all_unit [
+        process_user_cmd w ;
+        Pipe.iter r ~f:log_incoming
+      ]
+    end
 
 let () =
   Command.async ~summary:"Ftx WS client" begin
