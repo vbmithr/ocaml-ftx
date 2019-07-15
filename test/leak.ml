9fad9af3cc89968c323913eabb651f5649b4a6cb
Vincent Bernardoff <vb@luminar.eu.org>
2019-07-15 16:24:52 +0800

    add fd leak test

---
+++ test/leak.ml
@@ -0,0 +1,24 @@
+open Core
+open Async
+
+let rec inner = function
+  | 0 -> Deferred.unit
+  | n when n > 0 ->
+    Ftx_ws_async.with_connection_exn begin fun _r _w ->
+      Logs_async.app (fun m -> m "inner %d" n)
+    end >>= fun () ->
+    Clock_ns.after (Time_ns.Span.of_int_sec 3) >>= fun () ->
+    inner (pred n)
+  | _ -> invalid_arg "inner"
+
+let () =
+  Command.async ~summary:"Ftx leak test" begin
+    let open Command.Let_syntax in
+    [%map_open
+      let () = Logs_async_reporter.set_level_via_param None
+      and n = anon ("n" %: int) in
+      fun () ->
+        Logs.set_reporter (Logs_async_reporter.reporter ()) ;
+        inner n
+    ] end |>
+  Command.run
